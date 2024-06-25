2cacab7c72756de1960cc758eb9b1ec7ac3bbd10
quadcoreYT <pc_user_pro@outlook.com>
2024-06-25 13:51:06 +0300

    Now it is not needed

--- Create20GBimgfile.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-fsutil file createnew empty20GB.img 21474836480
-echo Empty 20 GB image file created as empty20GB.img
-
-echo IF YOU RAN THIS FILE AS ADMINISTRATOR,
-echo OUTPUT IS IN C:\Windows\system32
-
-pause
