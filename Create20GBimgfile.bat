0771c1ccadf7f7ddc500af66a37effb030c51b6b
pcuserproYT <pc_user_pro@outlook.com>
2024-06-28 07:46:29 +0300

    Creates 20 GB empty IMG file in file's directory (if ran as admin - C:\Windows\system32)

---
+++ Create20GBimgfile.bat
@@ -0,0 +1,8 @@
+@echo off
+fsutil file createnew empty20GB.img 21474836480
+echo Empty 20 GB image file created as empty20GB.img
+
+echo IF YOU RAN THIS FILE AS ADMINISTRATOR,
+echo OUTPUT IS IN C:\Windows\system32
+
+pause
